524d985e4778046fc4fe14e30971d599cbd9d7dc
Demon000 <demonsingur@gmail.com>
2023-10-20 23:18:19 +0530

    RM6785: init: Override the Dalvik heap configuration conditionally
    Dynamically load and override the Dalvik heap configuration based on the total RAM of the given device.
    
    Signed-off-by: prathamdubey2005 <134331217+prathamdubey2005@users.noreply.github.com>

--- init/init_RM6785.cpp
+++ init/init_RM6785.cpp
@@ -4,14 +4,20 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include <cstdlib>
+#include <fstream>
+#include <cstring>
+#include <sys/sysinfo.h>
 #include <android-base/file.h>
 #include <android-base/logging.h>
 #include <vector>
 
+#include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
 #include "vendor_init.h"
+#include "property_service.h"
 
 constexpr const char* kProcOperatorPath = "/proc/oppoVersion/operatorName";
 
@@ -125,8 +131,48 @@ void set_device() {
     }
 }
 
+void load_dalvik_properties() {
+    char const *heapstartsize;
+    char const *heapgrowthlimit;
+    char const *heapsize;
+    char const *heapminfree;
+    char const *heapmaxfree;
+    char const *heaptargetutilization;
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+
+    if (sys.totalram >= 5ull * 1024 * 1024 * 1024) {
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        heapstartsize = "16m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heaptargetutilization = "0.5";
+        heapminfree = "8m";
+        heapmaxfree = "32m";
+    } else if (sys.totalram >= 3ull * 1024 * 1024 * 1024) {
+        // from - phone-xhdpi-4096-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heaptargetutilization = "0.6";
+        heapminfree = "8m";
+        heapmaxfree = "16m";
+    } else {
+        return;
+    }
+
+    property_override("dalvik.vm.heapstartsize", heapstartsize);
+    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_override("dalvik.vm.heapsize", heapsize);
+    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_override("dalvik.vm.heapminfree", heapminfree);
+    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+}
+
 void vendor_load_properties() {
 #ifndef __ANDROID_RECOVERY__
     set_device();
+    load_dalvik_properties();
 #endif
 }
